a5c69e98c00c1f6c5cb762d2de74c8fb38642476
batuanr <88823692+batuanr@users.noreply.github.com>
2021-09-15 19:47:01 +0700

    ok

---
+++ src/bai.java
@@ -0,0 +1,17 @@
+public class bai {
+    public static void main(String[] args){
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        char c = 'a';
+        String s = "Hà Nội";
+        boolean b = true;
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
